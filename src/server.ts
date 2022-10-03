516ec731a0f111865295c794a238693ca5fa7729
Kaoru <kaoru.ronaldo@gmail.com>
2022-10-02 22:37:26 -0300

    fix: cors configuration

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,29 @@
 // Setup empty JS object to act as endpoint for all routes
-import express from "express"
-import { Application, Request, Response } from "express"
-import bodyParser from "body-parser"
+let projectData = {};
 
+// Require Express to run server and routes
+import express from "express"
+import { Application} from "express"
+
+import bodyParser from "body-parser"
+import cors from 'cors'
+
+// Start up an instance of app
 const app: Application = express();
 const port = 3000;
 
-let projectData;
-projectData = {};
-
-// Require Express to run server and routes
-
-// Start up an instance of app
-
 /* Middleware*/
+
+
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+app.use(cors())
 
 // Initialize the main project folder
 app.use(express.static('website'));
-
 
 // Setup Server
 app.listen(port, () => {
