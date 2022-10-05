90687eb6d0a16fb12e166f865ccc97bcc1ef57b1
KRK <kaoru.ronaldo@gmail.com>
2022-10-05 16:49:45 -0300

    Delete dist/utilities directory

--- dist/utilities/reader.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var projectData_1 = require("../data/projectData");
-var getWeatherJournalData = function (req, res, next) {
-    res.send(projectData_1.projectData);
-};
-exports.default = {
-    getWeatherJournalData: getWeatherJournalData
-};
