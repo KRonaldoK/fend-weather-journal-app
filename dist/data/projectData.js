b4fba1eacf1335f32c8ae936713faac3de32538f
Kaoru <kaoru.ronaldo@gmail.com>
2022-10-05 16:53:49 -0300

    chore: No source control for dist directory

--- dist/data/projectData.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.projectData = void 0;
-var projectData = {
-    date: "",
-    temperature: 0,
-    content: ""
-};
-exports.projectData = projectData;
